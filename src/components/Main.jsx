e2f5d9ec4514b26dcf48d24fca861c1a503e8d29
Kawal016 <kawalkamboj016@gmail.com>
2025-11-08 11:54:13 +0530

    Initail commit

---
+++ src/components/Main.jsx
@@ -0,0 +1,15 @@
+export default function Main()
+{
+    return(
+        <main className="main">
+            <h1 className="head1">Fun facts about React</h1>
+            <ul>
+                <li>Was first released in 2013</li>
+                <li>Was originally created by jordan walke</li>
+                <li>Has well over 100k star on github</li>
+                <li>Is maintained by meta</li>
+                <li>Power thousans of enterprise app, including mobile apps</li>
+            </ul>
+        </main>
+    )
+}
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+export default function Navbar()
+{
+    return(
+        <header>
+            <nav className="nav">
+             <img src="/src/assets/react.svg" className="nav-img" />
+             <span className="nav-span">ReactFacts</span>
+            </nav>
+        </header>
+    ) 
+}
